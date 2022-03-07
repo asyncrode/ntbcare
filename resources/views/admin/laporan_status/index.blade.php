93c3dcdb71922541773fe4b4ff14b650e464d09a
Fikri32 <fikrihaidar2032@gmail.com>
2022-03-07 21:48:53 +0700

    filter status

---
+++ resources/views/admin/laporan_status/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts_admin.master')
+@section('content')
+<div class="content">
+    <div class="block">
+        <div class="block-header block-header-default">
+            <h3 class="block-title">Laporan Pengaduan By Status</h3>
+        </div>
+        <div class="row ml-5">
+            <div class="col-md-3 mt-5">
+                <label for="">Status</label>
+                <select name="status" id="status" class="form-control mb-5">
+                    <option value="" class="bg-light" style="color: #282828">Pilih Status</option>
+                    <option value="Waiting" class="bg-light" style="color: #282828">Waiting</option>
+                    <option value="Approved" class="bg-light" style="color: #282828">Approved</option>
+                    <option value="Rejected" class="bg-light" style="color: #282828">Rejected</option>
+                    <option value="On process" class="bg-light" style="color: #282828">On Process</option>
+                    <option value="Complete" class="bg-light" style="color: #282828">Complete</option>
+                </select>
+            </div>
+            <div class="col-md-5 mt-5">
+                <label for="">Tanggal</label>
+                <div class="input-daterange input-group" id="datepicker">
+                    <input type="text" class="input-sm form-control" name="awal" id="awal" />
+                    <span class="input-group-addon">to</span>
+                    <input type="text" class="input-sm form-control" name="akhir" id="akhir" />
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="block-content block-content-full">
+
+
+            <!-- DataTables functionality is initialized with .js-dataTable-full-pagination class in js/pages/be_tables_datatables.min.js which was auto compiled from _es6/pages/be_tables_datatables.js -->
+            <table class="table table-bordered table-striped table-vcenter js-dataTable-full-pagination tableLaporan">
+                <thead>
+                    <tr>
+                        <th class="text-center" style="width: 5%">pelapor</th>
+                        <th class="text-center">pesan</th>
+                        <th class="d-none d-sm-table-cell" style="width: 15%;">kategori</th>
+                        <th class="d-none d-sm-table-cell" style="width: 15%;">status</th>
+                        <th class="d-none d-sm-table-cell" style="width: 15%;">terdaftar</th>
+                        <th class="text-center" style="width: 15%;">action</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
+
+@endsection
+@push('scripts')
+@include('admin.laporan_status.javascript')
+@endpush
---
+++ resources/views/admin/laporan_status/javascript.blade.php
@@ -0,0 +1,88 @@
+<script>
+    $(document).ready(function(){
+        var idEdit = 0;
+
+        // Show Data
+        var table = $('.tableLaporan').DataTable({
+            processing: true,
+            serverSide: true,
+            searching:  true,
+            dom: 'lBfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ],
+            ajax: {
+            url: "{{ route('laporan.data.status') }}",
+            data: function (d) {
+                    d.status = $('#status').val()
+                    d.awal = $('input[name=awal]').val();
+                    d.akhir = $('input[name=akhir]').val();
+                }
+            },
+            'columnDefs': [
+            {
+                "targets": [0,2,3,5], // your case first column
+                "className": "text-center"
+            }],
+            columns: [
+                {data: 'id_pelapor', name: 'id_pelapor'},
+                {data: 'pesan', name: 'pesan'},
+                {data: 'id_kategori', name: 'id_kategori'},
+                {data: 'status', name: 'status'},
+                {data: 'created_at', name: 'created_at'},
+                {data: 'action', name: 'action', orderable: false, searchable: false},
+            ]
+        });
+        // End Show
+
+        // Filter Select
+        $('#status').change(function(){
+            console.log('oke')
+            table.draw();
+        });
+        // End Filter
+
+
+        var awal, akhir;
+ 
+        // Custom filtering function which will search data in column four between two values
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                var min = awal.val();
+                var max = akhir.val();
+                var date = new Date( data[4] );
+        
+                if (
+                    ( min === null && max === null ) ||
+                    ( min === null && date <= max ) ||
+                    ( min <= date   && max === null ) ||
+                    ( min <= date   && date <= max )
+                ) {
+                    return true;
+                }
+                return false;
+            }
+        );
+ 
+        // Create date inputs
+        awal = $('#awal').datepicker({
+                    autoclose: true,
+                    todayHighlight: true,
+                    format: 'yyyy-mm-dd'
+            });
+        akhir = $('#akhir').datepicker({
+                    autoclose: true,
+                    todayHighlight: true,
+                    format: 'yyyy-mm-dd'
+            });
+    
+        // Refilter the table
+        $('#awal, #akhir').on('change', function () {
+            table.draw();
+        });
+
+
+       
+
+    })
+</script>
