7f4f59f525feccb36ef0db77aab537ade711af66
rifkimadhani <rifkimadhani@gmail.com>
2022-03-13 01:04:45 +0700

    Fix error untold story

--- resources/views/user/story/detail.blade.php
+++ resources/views/user/story/detail.blade.php
@@ -8,13 +8,13 @@
             <div class="content content-top text-center">
                 <div class="py-100">
                     
-                    <h1 class="font-w700 text-white mb-10">{{$untold->judul}}</h1>
+                    <h1 class="font-w700 text-white mb-10">{{$detail->judul}}</h1>
                     
                     {{-- <h2 class="h4 font-w400 text-white-op">Explore the world and provide value at the same time.</h2> --}}
                     <div class="font-size-md text-muted">
                         {{-- <a class="text-body-bg-dark" href="be_pages_generic_profile.html">John Smith</a> &bull; June 5, 2017 --}}
                         <span class="mr-15">
-                            <i class="fa fa-fw fa-calendar mr-5"></i><?= date('M d, Y', strtotime($untold->created_at)); ?>
+                            <i class="fa fa-fw fa-calendar mr-5"></i><?= date('M d, Y', strtotime($detail->created_at)); ?>
                         </span>
                     </div>
                 </div>
@@ -29,44 +29,25 @@
             <div class="col-lg-8">
                 <!-- Image -->
                 <div class="row no-gutters push js-gallery img-fluid-100 justify-content-center">
-                    @foreach ($gambar as $p)
+                    {{-- @foreach ($untold as $p)
+                    @if ($p->gambar->isEmpty())
+
+                    @else --}}
                     <div class="animated fadeIn text-center">
-                        <a class="img-link img-link-simple img-link-zoom-in img-lightbox" href="{{ asset('upload/untold_gambar/'.$p->gambar) }}">
-                            <img class="img-fluid" src="{{ asset('upload/untold_gambar/'.$p->gambar) }}" alt="">
+                        <a class="img-link img-link-simple img-link-zoom-in img-lightbox" href="{{ asset('upload/untold_gambar/'.$detail->gambar[0]['gambar']) }}">
+                            <img class="img-fluid" src="{{ asset('upload/untold_gambar/'.$detail->gambar[0]['gambar']) }}" alt="">
                         </a>
                     </div>
-                    @endforeach
+                    {{-- @endif
+                    @endforeach --}}
                     {{-- <div class="col-4 animated fadeIn">
                         <a class="img-link img-link-simple img-link-zoom-in img-lightbox" href="assets/media/photos/photo33@2x.jpg">
                             <img class="img-fluid" src="assets/media/photos/photo33.jpg" alt="">
                         </a>
-                    </div>
-                    <div class="col-4 animated fadeIn">
-                        <a class="img-link img-link-simple img-link-zoom-in img-lightbox" href="assets/media/photos/photo30@2x.jpg">
-                            <img class="img-fluid" src="assets/media/photos/photo30.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="col-4 animated fadeIn">
-                        <a class="img-link img-link-simple img-link-zoom-in img-lightbox" href="assets/media/photos/photo28@2x.jpg">
-                            <img class="img-fluid" src="assets/media/photos/photo28.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="col-6 animated fadeIn">
-                        <a class="img-link img-link-simple img-link-zoom-in img-lightbox" href="assets/media/photos/photo25@2x.jpg">
-                            <img class="img-fluid" src="assets/media/photos/photo25.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="col-6 animated fadeIn">
-                        <a class="img-link img-link-simple img-link-zoom-in img-lightbox" href="assets/media/photos/photo26@2x.jpg">
-                            <img class="img-fluid" src="assets/media/photos/photo26.jpg" alt="">
-                        </a>
                     </div> --}}
                 </div>
-                <!-- END Simple Gallery -->
-
-                <!-- Deskripsi-->
                 <p>
-                    {!!$untold->description!!}
+                    {!!$detail->description!!}
                 </p>
             </div>
         </div>
@@ -84,22 +65,25 @@
         <h2 class="content-heading border-t pt-10 ">Baca <small>Untold Story lainnya</small></h2>
         <div class="row ">
             <!-- Row #1 -->
-            @foreach ($gambarD as $g)
-                
+            @foreach ($untold as $g)
+            @if ($g->gambar->isEmpty())
+
+            @else
             <div class="col-md-6 col-xl-4">
-                <a class="block block-transparent border-left border-5x border-primary bg-image" style="background-image: url('{{asset("upload/untold_gambar/".$g->gambar)}}');" href="{{route('user.story.detail', $g->untold->id)}}"> {{--{{route('user.story.detail', $g->untold->id)}}--}}
+                <a class="block block-transparent border-left border-5x border-primary bg-image img-fluid" style="background-image: url('{{ asset('upload/untold_gambar/'.$g->gambar[0]['gambar']) }}');" href="{{route('user.story.detail', $g->id)}}; height: 200px; width:100%; object-fit:fill;"> 
                     <div class="block-content block-content-full bg-black-op">
                         <div class="pt-100">
-                            <h3 class="h4 text-white font-w700 mb-10">{{$g->untold->judul}}</h3>
+                            <h3 class="h4 text-white font-w700 mb-10">{{$g->judul}}</h3>
                             <h4 class="text-white-op font-size-default mb-0">
                                 <span class="mr-10">
-                                    <i class="fa fa-clock-o"></i> <?= date('M d, Y', strtotime($g->untold->created_at));?>
+                                    <i class="fa fa-clock-o"></i> <?= date('M d, Y', strtotime($g->created_at));?>
                                 </span>
                             </h4>
                         </div>
                     </div>
                 </a>
             </div>
+            @endif
 
             @endforeach
             {{-- <div class="col-md-6 col-xl-4">
